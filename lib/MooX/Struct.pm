ebe6278eccf78c361eca05f7fe78b4a2a9e4db6b
Toby Inkster <mail@tobyinkster.co.uk>
2012-12-18 21:55:50 +0000

    fix versions

--- lib/MooX/Struct.pm
+++ lib/MooX/Struct.pm
@@ -7,7 +7,7 @@ use utf8;
 
 BEGIN {
 	$MooX::Struct::AUTHORITY = 'cpan:TOBYINK';
-	$MooX::Struct::VERSION   = '0.008';
+	$MooX::Struct::VERSION   = '0.009';
 }
 
 use Moo          1.000000;
@@ -123,7 +123,7 @@ BEGIN {
 	{
 		no warnings;
 		our $AUTHORITY = 'cpan:TOBYINK';
-		our $VERSION   = '0.008';
+		our $VERSION   = '0.009';
 	}
 	
 	sub _uniq { my %seen; grep { not $seen{$_}++ } @_ };
