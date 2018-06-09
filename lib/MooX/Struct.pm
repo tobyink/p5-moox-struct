384d47c93aadf5b348182b29d1a4b0500becd11f
Toby Inkster <mail@tobyinkster.co.uk>
2018-06-09 18:04:16 +0100

    bump version, changelog

--- lib/MooX/Struct.pm
+++ lib/MooX/Struct.pm
@@ -7,7 +7,7 @@ use utf8;
 
 BEGIN {
 	$MooX::Struct::AUTHORITY = 'cpan:TOBYINK';
-	$MooX::Struct::VERSION   = '0.016';
+	$MooX::Struct::VERSION   = '0.017';
 }
 
 use Moo 1.000;
@@ -130,7 +130,7 @@ BEGIN {
 	{
 		no warnings;
 		our $AUTHORITY = 'cpan:TOBYINK';
-		our $VERSION   = '0.016';
+		our $VERSION   = '0.017';
 	}
 	
 	sub _uniq { my %seen; grep { not $seen{$_}++ } @_ };
